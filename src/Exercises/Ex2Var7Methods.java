6c5baeb6531e0264cd11a1dcf0a3d793615aaae3
Mihail <79074705+Kae1tas@users.noreply.github.com>
2021-09-07 15:03:33 +0900

    1

--- src/Exercises/Ex2Var7Methods.java
+++ src/Exercises/Ex2Var7Methods.java
@@ -8,29 +8,30 @@ public class Ex2Var7Methods {
     static Scanner sc = new Scanner(System.in);
 
     public static byte[] masLengthInput() {
+        System.out.println("Введите длину массива от 0 до 127:");
         try {
             return new byte[sc.nextByte()];
         } catch (NegativeArraySizeException e) {
             System.out.println("Отрицательный размер массива");
             return new byte[0];
         } catch (InputMismatchException e) {
-            System.out.println("Выход за пределы от -128 до 127 или несоответствие типу byte");
+            System.out.println("Несоответствие типу byte (выход за пределы от -128 до 127" + "\nили ввод неверных знаков (требуется ввод целых чисел)");
             return new byte[0];
         }
     }
 
 
     public static byte[] enter(byte[] mas) {
+        if (mas.length == 0) {
+            System.exit(1);
+        }
         try {
-            if (mas.length == 0) {
-                System.exit(1);
-            }
             for (byte i = 0; i < mas.length; i++) {
                 mas[i] = sc.nextByte();
             }
         } catch (InputMismatchException e) {
             System.out.println("Выход за пределы от -128 до 127 или несоответствие типу byte");
-            System.exit(1);
+            //System.exit(1);
         }
         return mas;
     }
